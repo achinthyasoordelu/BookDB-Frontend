94f1074bea25c72733d70678ffedf5c8b4e6f3ec
asoordelu3 <asoordel@purdue.edu>
2020-04-16 09:20:19 -0700

    New font, formatting updates on card

--- lib/Display/QuoteList.dart
+++ lib/Display/QuoteList.dart
@@ -23,6 +23,7 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteList extends State<QuoteList> {
+  static const int mdash = 0x2014;
   List<Quote> items = new List();
   bool isPerformingRequest = false;
   ScrollController _scrollController = new ScrollController();
@@ -70,34 +71,44 @@ class _QuoteList extends State<QuoteList> {
           if (index == items.length) {
             return _buildProgressIndicator();
           } else {
-            return Card(child: Column(
-              //TODO this should be dynamic. If I tag searched, I don't need tag in every card.
-              // If i title searched, don't need title everywhere. Common parts should be in
-              //header, only different bits should be here. Maybe export this to be "quotes card"
-              //"title card", "tag card", etc.
-              children: <Widget>[
-                Text(
-                    "Title: " + items[index].title + "   Author: " + items[index].author,
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      fontSize: 12
-                    ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                      items[index].quote,
-                      style: TextStyle(
-                        fontSize: 15
-                      ) //TODO
+            return Padding(
+                padding: EdgeInsets.all(10.0),
+                child:
+                  Card(
+                      child: Column(
+                      //TODO this should be dynamic. If I tag searched, I don't need tag in every card.
+                      // If i title searched, don't need title everywhere. Common parts should be in
+                      //header, only different bits should be here. Maybe export this to be "quotes card"
+                      //"title card", "tag card", etc.
+                        children: <Widget>[
+                          Text(
+                            items[index].title + String.fromCharCode(mdash) + items[index].author,
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 12,
+                              fontFamily: 'NotoSerif',
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(
+                                  items[index].quote,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'NotoSerif',
+                                  ) //TODO
+                              )
+                          ),
+                          Text(
+                              "Tags: "+ items[index].tags.toString(),
+                              style: TextStyle(
+                                fontSize: 8,
+                                fontFamily: 'NotoSerif',
+                              )
+                          ),
+                        ],
+                      )
                   )
-                ),
-                Text(
-                    "Tags: "+ items[index].tags.toString(),
-                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 0.7)
-                ),
-              ],
-            )
             );
           }
         },
@@ -117,92 +128,3 @@ class _QuoteList extends State<QuoteList> {
     );
   }
 }
-
-/*class _QuoteList extends State<QuoteList> {
-
-  final InfiniteScrollController _infiniteController = InfiniteScrollController(
-    initialScrollOffset: 0.0,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Example'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_downward),
-              onPressed: () {
-                _infiniteController.animateTo(_infiniteController.offset + 2000.0,
-                    duration: const Duration(milliseconds: 250), curve: Curves.easeIn);
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_upward),
-              onPressed: () {
-                _infiniteController.animateTo(_infiniteController.offset - 2000.0,
-                    duration: const Duration(milliseconds: 250), curve: Curves.easeIn);
-              },
-            ),
-          ],
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(text: 'First'),
-              Tab(text: 'Second'),
-              Tab(text: 'Third'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            build_scroll(),
-            build_scroll(),
-            build_scroll(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build_scroll() {
-    
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Results"),
-        ),
-        body: Center(
-          child: InfiniteListView.separated(
-              controller: _infiniteController,
-              itemBuilder: (BuildContext context, int index) {
-                return FutureBuilder<Quote>(
-                  future: fetchQuote(widget.queryURL),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return new Container();
-                    } else if (snapshot.hasData) {
-                      return Card(
-                          child: Column(
-                            children: <Widget>[
-                              Text("Title: " + snapshot.data.title),
-                              Text("Author: " + snapshot.data.author),
-                              Text("Quote: " + snapshot.data.quote),
-                              Text("Tags: " + snapshot.data.tags.toString()),
-                            ],
-                          )
-                      );
-                    } else {
-                      return LinearProgressIndicator();
-                    }
-                  },
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                const Divider(height: 2.0), anchor: 0.5,
-          )
-        )
-    );
-  }
-}*/
